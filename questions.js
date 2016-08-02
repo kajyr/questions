09a8d9790cfaaea1b07d8ff7b6a60b77da38d5c4
Carlo Panzi <carlo.panzi@gmail.com>
2016-08-02 17:48:25 +0200

    default values

--- questions.js
+++ questions.js
@@ -2,15 +2,20 @@ module.exports = function (){
 
 	var obj = {};
 
-	obj.askMany = function (questions, callback) {
+	obj.askMany = function (questions, defaultValues, callback) {
 
 		var response = {};
+
+		if (arguments.length == 2 && typeof arguments[1] === 'function') {
+			callback = arguments[1];
+			defaultValues = {};
+		}
 
 		var pool = function(){
 
 			for (i in questions)
 			{
-				obj.askOne( questions[i], function(data){ 
+				obj.askOne( questions[i], defaultValues[i], function(data){ 
 					response[i] = data 
 					delete questions[i]
 					pool()
@@ -25,19 +30,27 @@ module.exports = function (){
 		pool();
 	}
 
-	obj.askOne = function (question, callback) {
+	obj.askOne = function (question, defaultValue, callback) {
 		var stdin = process.stdin, 
 			stdout = process.stdout
+
+		if (arguments.length == 2 && typeof arguments[1] === 'function') {
+			callback = arguments[1];
+			defaultValue = '';
+		}
 
 		stdin.resume()
 		stdout.write( (question.required == false ? '(Optional) ': '') + question.info + ": ");
 
 		stdin.once('data', function(data) {
 			result = data.toString().trim();
+			if (result == '' && defaultValue) {
+				result = defaultValue;
+			}
 
 			if (question.required != false && result == '') {
 				// Ask again
-				obj.askOne(question, callback);
+				obj.askOne(question, defaultValue, callback);
 			} else {
 				// Return result
 				stdin.pause();
